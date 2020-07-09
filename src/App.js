5def3769787a937834ec26dfb7e89e56a583235a
rokas <rokas.gegzna@gmail.com>
2020-07-09 20:10:44 +0300

    slider is done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import FirstPage from "./screens/firstPage";
 import SecondPage from "./screens/secondPage";
 import ThirdPage from "./screens/thirdPage";
--- src/screens/secondPage.js
+++ src/screens/secondPage.js
@@ -1,28 +1,65 @@
 import React, { Component } from "react";
-
+import { FaBeer, FaAlignLeft, FaArrowAltCircleLeft, FaArrowRight, FaArrowAltCircleRight } from 'react-icons/fa';
+const TESTDATA = [{
+  context: "vienas",
+  id: 0
+}, {
+  context: "du",
+  id: 1
+}, {
+  context: "trys",
+  id: 2
+}, {
+  context: "keturi",
+  id: 3
+}];
 class SecondPage extends Component {
   constructor() {
     super();
     this.state = {
-      items: ["vienas", "du", "trys", "keturi"],
+      itemsList: TESTDATA,
+      item: TESTDATA[0],
     };
   }
   componentDidMount() {
     console.log("Component About has mounted");
+    setInterval(() => {
+      if (this.state.itemsList.length - 1 === this.state.item.id) { return this.setState({ item: TESTDATA[0] }) }
+      this.goToNextItem();
+    }, 3000);
   }
   componentWillUnmount() {
     console.log("Component About has unmounted");
   }
+  goToPreviousItem = () => {
+    console.log('HHHAHAH')
+    if (this.state.item.id === 0)
+      return null;
+    const newIndex = this.state.item.id - 1;
+    this.setState({ item: this.state.itemsList[newIndex] })
+  }
+  goToNextItem = () => {
+    console.log('HHHAHAH')
+    if (this.state.item.id === this.state.itemsList.length - 1)
+      return null;
+    const newIndex = this.state.item.id + 1;
+    this.setState({ item: this.state.itemsList[newIndex] })
+  }
   render() {
     return (
-      <div className="about-layout" style={{ backgroundColor: "lightblue" }}>
-        <div className="lovable-list-slider">
-          <div className="lovable-list-wrapper">
-            {this.state.items.map((item) => {
-              return <div className="lovable-list-item">{item}</div>;
+      <div className="about-layout" style={{ backgroundColor: "cyan" }}>
+        <div className={`lovable-list-slider active-slider-${this.state.item.id}`}>
+          <div className="lovable-list-wrapper" style={{ 'transform': `translateX(-${this.state.item.id * (100 / this.state.itemsList.length)}%)` }}>
+            {this.state.itemsList.map((item) => {
+              return <div id={`lovable-list-item-${item.id}`} className="lovable-list-item" key={item.id}>{item.context}</div>;
             })}
           </div>
         </div>
+        <div className='lovable-list-button-wrapper'>
+          <div className='lovable-list-button' onClick={this.goToNextItem}>	<FaArrowAltCircleRight /></div>
+          <div className='lovable-list-button2' onClick={this.goToPreviousItem}>	<FaArrowAltCircleLeft /></div>
+        </div>
+
       </div>
     );
   }
